cb0dfca00183f988e6ace2a3ee4812e000397ef1
Vijay Menon <vijaymenonx@gmail.com>
2021-04-05 15:51:16 -0700

    classes

--- user.rb
+++ user.rb
@@ -1,7 +1,18 @@
 class User 
+    #shortcut for writing getters/setters
+    attr_accessor :name
     def initialize(name)
         @name = name
     end
+
+    #getters/setters
+    def get_name
+        @name
+    end
+    def set_name=(name)
+        @name = name
+    end
+
     def run
         puts "Hey I'm running" 
     end 
@@ -14,6 +25,15 @@ user2 = User.new("Kevin")
 #puts User.ancestors
 puts user
 
-user.run
-user1.run
-user2.run
+# user.run
+# user1.run
+# user2.run
+
+#getter
+# puts user.get_name
+# user.set_name = "John"
+
+# w/attr_accessor
+puts user.name
+user.name = "Winston"
+puts user.name
